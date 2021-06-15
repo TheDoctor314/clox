c438716e56134fca6f94e1e36404d9be3629db95
TheDoctor314 <darsh.rajan95@gmail.com>
2021-06-15 17:28:18 +0530

    Comment out diagnostic flags

--- src/common.h
+++ src/common.h
@@ -5,11 +5,11 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define DEBUG_TRACE_EXEC
-#define DEBUG_PRINT_CODE
+//#define DEBUG_TRACE_EXEC
+//#define DEBUG_PRINT_CODE
 
 // This flags allows the GC to run as often as possible
-#define DEBUG_STRESS_GC
-#define DEBUG_LOG_GC
+//#define DEBUG_STRESS_GC
+//#define DEBUG_LOG_GC
 
 #endif
