b229e7e4e8c9fa2aa7d8b83d73d03b0d9c3d87c4
TheDoctor314 <darsh.rajan95@gmail.com>
2021-06-19 18:31:49 +0530

    Add ObjBoundMethod type
    This object tracks is a method type. It tracks the instance to which it
    is bound. During runtime, the VM will connect 'this' to the 'receiver'
    field of ObjBoundMethod.

--- src/object.h
+++ src/object.h
@@ -6,21 +6,23 @@
 #include "table.h"
 #include "value.h"
 
-#define OBJ_TYPE(value)     (AS_OBJ(value)->type)
-#define IS_STRING(object)   is_obj_type(object, OBJ_STRING)
-#define IS_FUNC(object)     is_obj_type(object, OBJ_FUNC)
-#define IS_CLOSURE(object)  is_obj_type(object, OBJ_CLOSURE)
-#define IS_NATIVE(object)   is_obj_type(object, OBJ_NATIVE)
-#define IS_CLASS(object)    is_obj_type(object, OBJ_CLASS)
-#define IS_INSTANCE(object) is_obj_type(object, OBJ_INSTANCE)
+#define OBJ_TYPE(value)      (AS_OBJ(value)->type)
+#define IS_STRING(obj)       is_obj_type(obj, OBJ_STRING)
+#define IS_FUNC(obj)         is_obj_type(obj, OBJ_FUNC)
+#define IS_CLOSURE(obj)      is_obj_type(obj, OBJ_CLOSURE)
+#define IS_NATIVE(obj)       is_obj_type(obj, OBJ_NATIVE)
+#define IS_CLASS(obj)        is_obj_type(obj, OBJ_CLASS)
+#define IS_INSTANCE(obj)     is_obj_type(obj, OBJ_INSTANCE)
+#define IS_BOUND_METHOD(obj) is_obj_type(obj, OBJ_BOUND_METHOD)
 
-#define AS_STRING(value)   ((ObjString *)AS_OBJ(value))
-#define AS_CSTRING(value)  (((ObjString *)AS_OBJ(value))->chars)
-#define AS_FUNC(value)     ((ObjFunction *)AS_OBJ(value))
-#define AS_CLOSURE(value)  ((ObjClosure *)AS_OBJ(value))
-#define AS_NATIVE(value)   (((ObjNativeFunc *)AS_OBJ(value))->func)
-#define AS_CLASS(value)    ((ObjClass *)AS_OBJ(value))
-#define AS_INSTANCE(value) ((ObjInstance *)AS_OBJ(value))
+#define AS_STRING(val)       ((ObjString *)AS_OBJ(val))
+#define AS_CSTRING(val)      (((ObjString *)AS_OBJ(val))->chars)
+#define AS_FUNC(val)         ((ObjFunction *)AS_OBJ(val))
+#define AS_CLOSURE(val)      ((ObjClosure *)AS_OBJ(val))
+#define AS_NATIVE(val)       (((ObjNativeFunc *)AS_OBJ(val))->func)
+#define AS_CLASS(val)        ((ObjClass *)AS_OBJ(val))
+#define AS_INSTANCE(val)     ((ObjInstance *)AS_OBJ(val))
+#define AS_BOUND_METHOD(val) ((ObjBoundMethod *)AS_OBJ(val))
 
 typedef enum {
     OBJ_STRING,
@@ -30,6 +32,7 @@ typedef enum {
     OBJ_NATIVE,
     OBJ_CLASS,
     OBJ_INSTANCE,
+    OBJ_BOUND_METHOD,
 } ObjType;
 
 struct Obj {
@@ -79,6 +82,12 @@ typedef struct {
     Table fields;
 } ObjInstance;
 
+typedef struct {
+    Obj obj;
+    Value receiver;
+    ObjClosure *method;
+} ObjBoundMethod;
+
 typedef Value (*NativeFn)(int arg_count, Value *args);
 
 typedef struct {
@@ -94,6 +103,7 @@ ObjFunction *newFunction();
 ObjClosure *newClosure(ObjFunction *func);
 ObjClass *newClass(ObjString *name);
 ObjInstance *newInstance(ObjClass *klass);
+ObjBoundMethod *newBoundMethod(Value receiver, ObjClosure *method);
 
 ObjUpvalue *newUpvalue(Value *slot);
 ObjNativeFunc *newNative(NativeFn func);
